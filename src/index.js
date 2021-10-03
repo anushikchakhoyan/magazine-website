f2fd62be98020d8d2e5784fcc67e69f891e8a784
Anush Chakhoyan <anush.chakhoyan@sabx.com>
2021-10-03 20:15:28 +0400

    removed unused line of code

--- src/index.js
+++ src/index.js
@@ -18,4 +18,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-// 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
