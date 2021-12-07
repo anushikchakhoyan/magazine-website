1ecc2de24955bc72bff130638567afce034d2d6b
Anush Chakhoyan <anush.chakhoyan@inspace.chat>
2021-12-08 00:36:04 +0400

    test deployment

---
+++ src/components/Layout/Footer/SocialLinks/index.jsx
@@ -0,0 +1,13 @@
+import {useTranslation} from "react-i18next";
+
+const SocialLinks = () => {
+    const {t} = useTranslation();
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default SocialLinks;
